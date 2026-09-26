c051ae725bdb31d60a3e60b19cf1fcbfceccbcc1
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-09-26 23:22:52 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -5,21 +5,21 @@ set_warnings("all", "error")
 
 add_requires("vcpkg::spdlog", {debug = is_mode("debug"), configs = {shared = true}})
 
-target("Bamboo")
+add_defines("NOMINMAX", "UNICODE", "_UNICODE", "_CONSOLE")
+
+add_cxxflags("/JMC", "/sdl", "/permissive-", "/Zc:preprocessor", {tools = "cl", force = true})
+
+if is_mode("release") then
+    set_symbols("debug")
+    set_policy("build.optimization.lto", true)
+    add_cxxflags("/Oi", "/Gy", {tools = "cl"})
+end
+
+target("bamboo")
     set_kind("binary")
     add_files("src/**.cpp", "src/**.ixx")
     add_packages("vcpkg::spdlog")
 
-    add_defines("NOMINMAX", "UNICODE", "_UNICODE", "_CONSOLE")
-
-    add_cxxflags("/JMC", "/sdl", "/permissive-", "/Zc:preprocessor", "/external:W0", {tools = "cl", force = true})
-
     set_targetdir(is_mode("debug") and "build/debug" or "build/release")
     set_rundir("tests")
     set_runargs("game.mfa")
-
-    if is_mode("release") then
-        set_symbols("debug")
-        add_cxxflags("/Oi", "/Gy", {tools = "cl"})
-        set_policy("build.optimization.lto", true)
-    end
